33aa1764c0a07566f14efb30a88a21e4db7f0782
Void491111 <voidw87@gmail.com>
2026-05-03 15:33:08 +0700

    melakukan routing dan testing untuk login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-ecenter">
+            <h1 className="text-2xl font-bold">Login Page (placeholder)</h1>
+        </div>
+    );
+}
---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,9 @@
+export default function MainLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="min-h-screen">{children}</div>
+    )
+}
