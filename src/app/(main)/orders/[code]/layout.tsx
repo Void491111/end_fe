4d609a6582493252d8691bba352c2b3ec52e91ab
unknown <voidw87@gmail.com>
2026-07-18 00:31:25 +0700

    membuat folder [code] dan membuat file layout.tsx di dalamnya

---
+++ src/app/(main)/orders/[code]/layout.tsx
@@ -0,0 +1,11 @@
+export default function CustomerOrderLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-background">
+      {children}
+    </div>
+  );
+}
