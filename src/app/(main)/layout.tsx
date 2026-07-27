dc424791bacee4467046ef45afd6fb6ca8dc0f99
unknown <voidw87@gmail.com>
2026-07-27 07:28:57 +0700

    update

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/layout/Header";
 import { PageTransition } from "@/components/shared/PageTransition";
 import { AuthGuard } from "@/components/auth/AuthGuard";
+import { CashierSplashHost } from "@/components/splash/CashierSplashHost";
 
 export default function MainLayout({
   children,
@@ -9,6 +10,7 @@ export default function MainLayout({
 }) {
   return (
     <AuthGuard>
+      <CashierSplashHost />
       <div className="flex h-screen flex-col bg-background">
         <Header />
         <main className="flex flex-1 overflow-auto">
